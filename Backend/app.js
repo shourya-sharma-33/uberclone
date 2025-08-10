d9274c43308e4a3653afeb1e5fc6d7bc9bb7249e
shourya-sharma-33 <shourya_ml_dl_artificial_intelligence@proton.me>
2025-08-10 18:12:46 +0530

    आरंभ start

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('hello world');
+})
+
+module.exports = app;
