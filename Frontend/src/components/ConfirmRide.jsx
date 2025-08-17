97de06661570eadbb417a99503e4e39e96297b44
shourya-sharma-33 <shourya_ml_dl_artificial_intelligence@proton.me>
2025-08-18 01:11:37 +0530

    Adjust ConfirmRide panel position for improved visibility

--- Frontend/src/components/ConfirmRide.jsx
+++ Frontend/src/components/ConfirmRide.jsx
@@ -9,6 +9,20 @@ const ConfirmRide = () => {
             <i className='text-3xl text-gray-200 ri-arrow-down-wide-line'></i>
         </h5>
         <h3 className="text-2xl font-semibold mb-5">Confirm your Ride</h3>
+
+        <div className='flex justify-between flex-col items-center'>
+              <img
+                  src="https://toppng.com/public/uploads/preview/car-png-11545045307loxqxkku4n.png"
+                  alt="Uber Car"
+                  className="object-contain"
+              />
+              <div className='w-full'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <button className='w-full bg-green-400 text-white font-semibold p-2 rounded-sm'>Confirm</button>
+        </div>
     </div>
   )
 }
