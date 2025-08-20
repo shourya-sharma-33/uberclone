df976022bf3c6b4f7ccfb6f2edbe7fb9a1741ffd
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-08-20 16:31:15 +0530

    Refactor layout in CaptainHome component for improved structure and styling

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -11,13 +11,13 @@ const CaptainHome = () => {
           <i className="text-lg font-bold ri-logout-box-r-line"></i>
         </Link>
       </div>
-      <div className='h-1/2'>
+      <div className='h-2/3'>
         <img src={Bg2} alt="" className='h-full w-full object-cover' />
       </div>
-      <div className='h-1/2 p-4'>
+      <div className='h-1/3 p-6 flex flex-col gap-5'>
         <div className='flex items-center justify-between gap-3'>
-          <div className='flex items-center justify-between '>
-            <img className='h-10 w-10' src={Bg2} alt="" />
+          <div className='flex items-center justify-between gap-2'>
+            <img className='h-16 w-16 rounded-full' src={Bg2} alt="" />
             <h4 className='text-lg font-medium'>harsh patel</h4>
           </div>
           <div>
@@ -25,12 +25,13 @@ const CaptainHome = () => {
             <p className='text-sm text-gray-600'>Earned</p>
           </div>
         </div>
-        <div className='flex justify-center gap-5 items-start'>
+        <div className='flex justify-between gap-5 items-start'>
           <div className='text-center'>
             <i className="text-2xl font-thin ri-timer-2-line"></i>
             <h5 className='text-lg font-medium'>10.2</h5>
             <p className='text-sm text-gray-600'>Hour Online</p>
           </div>
+            
           <div className='text-center'>
             <i className="text-2xl font-thin ri-speed-up-fill  "></i>
             <h5 className='text-lg font-medium'>10.2</h5>
