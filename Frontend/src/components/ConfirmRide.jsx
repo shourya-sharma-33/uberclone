6360d318b497626a6e65edd9975112c745192028
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-08-21 02:34:24 +0530

    Add FinishRide component and implement finish ride panel in CaptainRiding with GSAP animations

--- Frontend/src/components/ConfirmRide.jsx
+++ Frontend/src/components/ConfirmRide.jsx
@@ -42,7 +42,6 @@ const ConfirmRide = (props) => {
             <button onClick={() => {
                   props.setVehicleFound(true)
                   props.setConfirmRidePanel(false)
-                  console.log("hello")
             }} className='w-full mt-5 bg-green-400 text-white font-semibold p-2 rounded-sm'>Confirm</button>
         </div>
     </div>
