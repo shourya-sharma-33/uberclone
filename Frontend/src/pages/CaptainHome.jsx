2f3e7285bc953cf9073e555c38e86d6357efcb0b
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-08-20 16:42:25 +0530

    Add CaptainDetails component to CaptainHome for enhanced user information display

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PlaceHolder from "../assets/pl2.jpeg"
 import Bg2 from "../assets/placeholder.jpg"
+import CaptainDetails from '../components/CaptainDetails'
 const CaptainHome = () => {
   return (
     <div className='h-screen '>
@@ -14,36 +15,8 @@ const CaptainHome = () => {
       <div className='h-3/4'>
         <img src={Bg2} alt="" className='h-full w-full object-cover' />
       </div>
-      <div className='h-1/4 p-6 flex flex-col gap-5'>
-        <div className='flex items-center justify-between gap-3'>
-          <div className='flex items-center justify-between gap-2'>
-            <img className='h-16 w-16 rounded-full' src={Bg2} alt="" />
-            <h4 className='text-lg font-medium'>harsh patel</h4>
-          </div>
-          <div>
-            <h4 className='text-xl font-semibold'>$12</h4>
-            <p className='text-sm text-gray-600'>Earned</p>
-          </div>
-        </div>
-        <div className='flex justify-between gap-5 items-start'>
-          <div className='text-center'>
-            <i className="text-2xl font-thin ri-timer-2-line"></i>
-            <h5 className='text-lg font-medium'>10.2</h5>
-            <p className='text-sm text-gray-600'>Hour Online</p>
-          </div>
-            
-          <div className='text-center'>
-            <i className="text-2xl font-thin ri-speed-up-fill  "></i>
-            <h5 className='text-lg font-medium'>10.2</h5>
-            <p className='text-sm text-gray-600'>Hour Online</p>
-          </div>
-          <div className='text-center'>
-            <i className="text-2xl font-thin ri-booklet-line"></i>
-            <h5 className='text-lg font-medium'>10.2</h5>
-            <p className='text-sm text-gray-600'>Hour Online</p>
-          </div>
-        </div>
-
+      <div className='h-1/4'>
+        <CaptainDetails/>
       </div>
     </div>
   )
