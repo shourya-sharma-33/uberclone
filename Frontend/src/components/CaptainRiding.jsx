5464357cf625c3ab449dd4438eb5acb9eb10e98a
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-08-21 14:00:18 +0530

    final frontend ho gaya i guess

--- Frontend/src/components/CaptainRiding.jsx
+++ Frontend/src/components/CaptainRiding.jsx
@@ -4,11 +4,13 @@ import FinishRide from './FinishRide'
 import { useGSAP } from '@gsap/react'
 import { gsap } from "gsap";
 
-
 const CaptainRiding = () => {
-
     const [finishRidePanel, setFinishRidePanel] = useState(false)
+    const [ridingPanel, setRidingPanel] = useState(false)
     const finishRidePanelRef = useRef()
+    const ridingPanelRef = useRef()
+    const iconRef = useRef()
+
     useGSAP(function () {
         if (finishRidePanel) {
             gsap.to(finishRidePanelRef.current, {
@@ -21,13 +23,26 @@ const CaptainRiding = () => {
         }
     }, [finishRidePanel])
 
+    useGSAP(function () {
+        if (ridingPanel) {
+            gsap.to(ridingPanelRef.current, {
+                transform: 'translateY(-70%)'
+            })
+            gsap.to(iconRef.current, { rotate: 180 }) 
+        } else {
+            gsap.to(ridingPanelRef.current, {
+                transform: 'translateY(3%)'
+            })
+            gsap.to(iconRef.current, { rotate: 0 }) 
+        }
+    }, [ridingPanel])
 
     return (
         <div className='h-screen relative'>
             <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
                 <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
                 <Link to='/captain-home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
-                    <i className='text-lg font-medium ri-logout-box-line'></i>
+                    <i className='text-lg font-medium ri-logout-box-line '></i>
                 </Link>
             </div>
 
@@ -35,17 +50,22 @@ const CaptainRiding = () => {
                 <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gMx05pgIIShbfnX.gif" alt="" />
             </div>
 
-            <div className='h-1/5 p-6 flex items-center justify-between relative bg-yellow-400 pt-10'>
+            <div ref={ridingPanelRef} className='h-screen p-6 items-start justify-between relative bg-yellow-400 pt-10'>
                 <h5 className='p-1 text-center w-[90%] absolute top-0' onClick={() => {
-                    setFinishRidePanel(true)
-                 }}>
-                    <i className='text-3xl text-gray-800 ri-arrow-up-wide-line'></i>
+                    setRidingPanel(!ridingPanel)
+                }}>
+                    <i ref={iconRef} className='text-3xl text-gray-800 ri-arrow-up-wide-line'></i>
                 </h5>
-                <h4 className='text-xl font-semibold'>4 KM away</h4>
-                <button className='bg-green-600 text-white font-semibold px-10 py-3 rounded-lg'>Complete Ride</button>
+                <div className='w-full flex justify-around items-center my-5 mb-10'>
+                    <h4 className='text-xl font-semibold'>4 KM away</h4>
+                    <button className='bg-green-600 text-white font-semibold px-10 py-3 rounded-lg' onClick={() => {
+                        setFinishRidePanel(true)
+                    }}>Complete Ride</button>
+                </div>
+                <div className='bg-white h-full w-full'></div>
             </div>
             <div ref={finishRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-                <FinishRide />
+                <FinishRide setFinishRidePanel={setFinishRidePanel} />
             </div>
         </div>
     )
